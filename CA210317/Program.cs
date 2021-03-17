0ed189c046fef6c1324a40425273d58e45a74e3f
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2021-03-17 18:33:12 +0100

    add progtetelek

--- CA210317/Program.cs
+++ CA210317/Program.cs
@@ -11,12 +11,146 @@ namespace CA210317
     {
         static Random rnd = new Random();
 
+        static int[] tomb = { 32, 11, 2, 20, 61, 82, 17, 69, 907, 2 };
+
         static void Main()
         {
-            Fealdatok05_01_3();
+            //ProgtetelekSorozatszamitas();
+            //ProgtetelekEldontesLinKerKivalasztas();
+            ProgtetelekSzelsoertek();
+
             Console.ReadKey();
         }
 
+        private static void ProgtetelekSorozatszamitas()
+        {
+            int sum = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                sum += tomb[i];
+            } 
+            Console.WriteLine($"Tömb elemeinek összege: {sum}");
+            Console.WriteLine($"Tömb elemeinek átlaga: {sum/(float)tomb.Length}");
+
+            string[] szavak = { "meg", "szent", "ség", "telenít", "hetetlen", "ség", "eiek", "ért" };
+            string con = "";
+            for (int i = 0; i < szavak.Length; i++)
+            {
+                con += szavak[i];
+            }
+            Console.WriteLine(con);
+
+            long mult = 1;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                mult *= tomb[i];
+            }
+            Console.WriteLine("Tömb elemeinek szorzata: {0:N0}", mult);
+
+            Console.WriteLine($"Összeg: {tomb.Sum()}");
+            Console.WriteLine($"Átlag: {tomb.Average()}");
+        }
+        private static void ProgtetelekEldontesLinKerKivalasztas()
+        {
+            //Eldöntés:
+            //INPUT -> sorozat
+            //KÉRÉS <T> tulajdonságnak megfelel-e
+            //OUTPUT bool
+
+            //pl.: T:: megtalálható benne a 66-os szám
+            int i = 0;
+            while (i < tomb.Length && tomb[i] != 11)
+            {
+                i++;
+            }
+
+            if (i < tomb.Length) Console.WriteLine("benne van");
+            else Console.WriteLine("nincs benne");
+
+            Console.WriteLine("----------------------");
+            //Lineáris Keresés: (rendezetlen adatszerkezetekre alkalmazom)
+            //INPUT -> sorozat
+            //KÉRÉS -> adott elem szerepel-e, és ha igen, akkor hol?
+            //OUTPUT index
+            i = 0;
+            while (i < tomb.Length && tomb[i] != 69)
+            {
+                i++;
+            }
+
+            if (i < tomb.Length)
+            {
+                Console.WriteLine("benne van");
+                Console.WriteLine($"idenx: {i}"); //<-
+                Console.WriteLine($"hely: {i + 1}");
+            }
+            else
+            {
+                Console.WriteLine("nincs benne");
+                Console.WriteLine("index: -1"); //<-
+            }
+
+            Console.WriteLine("----------------------");
+            //Kiválasztás: 100%-ra tudom, hogy benne van, de azt nem, hogy hol
+            //INPUT -> sorozat
+            //KÉRÉS -> hol van a keresett elem
+            //OUTPUT index
+            i = 0;
+            while (tomb[i] != 20)
+            {
+                i++;
+            }
+            Console.WriteLine($"index: {i}");
+
+
+            int linker = Array.IndexOf(tomb, 2);
+            Console.WriteLine(linker);
+
+            bool eldontes = tomb.Contains(2000);
+            Console.WriteLine(eldontes);
+
+        }
+        private static void ProgtetelekSzelsoertek()
+        {
+            //rendezetlen adatsoron értelmezzük a szélsőérték tételeket
+
+            //mimimum VALUE -> legkisebb érték
+            //minimum INDEX -> legkisebb érték indexe <--
+            //minimum LOCATION -> lekgisebb érték helye (index + 1)
+            //maximum VALUE -> legnagyobb érték
+            //maximum INDEX -> legnagyobb érték indexe <--
+            //maximum LOCATION -> legnagyobb érték helye (index + 1)
+
+
+            int mini = 0;
+            for (int i = 1; i < tomb.Length; i++)
+            {
+                if (tomb[i] < tomb[mini]) mini = i;
+            }
+
+            Console.WriteLine($"mimimum value: {tomb[mini]}");
+            Console.WriteLine($"minimum index: {mini}");
+            Console.WriteLine($"minimum location: {mini + 1}");
+
+            Console.WriteLine("-----------------------------");
+
+            int maxi = 0;
+            for (int i = 1; i < tomb.Length; i++)
+            {
+                if (tomb[i] > tomb[maxi]) maxi = i;
+            }
+
+            Console.WriteLine($"maximum value: {tomb[maxi]}");
+            Console.WriteLine($"maximum index: {maxi}");
+            Console.WriteLine($"maximum location: {maxi + 1}");
+
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine("minimum: " + tomb.Min());
+            Console.WriteLine("maximum: " + tomb.Max());
+            Console.WriteLine("minindex: " + Array.IndexOf(tomb, tomb.Min()));
+            Console.WriteLine("maxindex: " + Array.IndexOf(tomb, tomb.Max()));
+        }
         private static void Fealdatok05_01_3()
         {
             Console.CursorVisible = false;
