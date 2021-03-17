b4a5c28b04dbb2e8b3036f499cf8efc83075553b
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2021-03-17 17:38:31 +0100

    add big fkin race

--- CA210317/Program.cs
+++ CA210317/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CA210317
@@ -12,10 +13,117 @@ namespace CA210317
 
         static void Main()
         {
-            Jelszo();
+            Fealdatok05_01_3();
             Console.ReadKey();
         }
 
+        private static void Fealdatok05_01_3()
+        {
+            Console.CursorVisible = false;
+            int csillagPont = 0;
+            int kukacPont = 0;
+
+            for (int k = 0; k < 3; k++)
+            {
+                int csillag = 0;
+                int kukac = 0;
+                while (csillag <= 50 && kukac <= 50)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{k + 1}. futam:");
+
+                    csillag += rnd.Next(1, 4);
+                    for (int i = 0; i < csillag; i++) Console.Write(' ');
+                    Console.Write('*');
+
+                    Console.Write("\n\n");
+
+                    kukac += rnd.Next(1, 4);
+                    for (int i = 0; i < kukac; i++) Console.Write(' ');
+                    Console.Write('@');
+
+                    Thread.Sleep(100);
+                }
+
+                if (csillag == kukac) Console.WriteLine("\n\ndöntetlen!");
+                else if (csillag > kukac)
+                {
+                    Console.WriteLine("\n\n* nyert");
+                    csillagPont++;
+                }
+                else
+                {
+                    Console.WriteLine("\n\n@ nyert!");
+                    kukacPont++;
+                }
+
+                Console.WriteLine($"\nÖsszesítés:\n@: {kukacPont} pont\n*: {csillagPont} pont");
+
+                while (Console.ReadKey(true).Key != ConsoleKey.Enter) ;
+            }
+            Console.Clear();
+            Console.WriteLine(":::VÉGEREDMÉNY:::");
+            Console.WriteLine($"@: {kukacPont} pont\t|||\t*: {csillagPont} pont");
+            if(csillagPont == kukacPont) Console.WriteLine("döntetlen");
+            else if (csillagPont > kukacPont) Console.WriteLine("* győzött!!!!44négy!4!!");
+            else Console.WriteLine("@ győzött!!!!44négy!4!!");
+
+
+        }
+        private static void Fealdatok05_01_2()
+        {
+            Console.CursorVisible = false;
+            int x = 0;
+            int y = 0;
+
+            while (x <= 50 && y <= 50)
+            {
+                Console.Clear();
+
+                //*
+                x += rnd.Next(1, 4);
+                for (int i = 0; i < x; i++) Console.Write(' ');
+                Console.Write('*');
+
+                Console.Write("\n\n");
+
+                //@
+                y += rnd.Next(1, 4);
+                for (int i = 0; i < y; i++) Console.Write(' ');
+                Console.Write('@');
+
+                Thread.Sleep(300);
+            }
+
+            if (x == y) Console.WriteLine("\n\ndöntetlen!");
+            else if (x > y) Console.WriteLine("\n\n* nyert");
+            else Console.WriteLine("\n\n@ nyert!");
+        }
+        private static void Fealdatok05_01_1()
+        {
+            int x = 0;
+
+            while (x <= 50)
+            { 
+                Console.Clear();
+                //Console.BackgroundColor = ConsoleColor.Green;
+                for (int i = 0; i < x; i++) Console.Write(' ');
+                //Console.ResetColor();
+                Console.Write("*");
+                Thread.Sleep(300);
+                x += rnd.Next(1, 4);
+            }
+        }
+        private static void Fealdatok05_01_0()
+        {
+            int x = 0;
+
+            while (x <= 50)
+            {              
+                Console.WriteLine(x);
+                x += rnd.Next(1, 4);
+            }
+        }
         private static void Jelszo()
         {
             string jelszo = "PassWord123";
@@ -32,7 +140,6 @@ namespace CA210317
             if (bekert == jelszo) Console.WriteLine("sikeresen bejelentkeztél!");
             else Console.WriteLine("túl sok próbálkozás, próbáld később");           
         }
-
         private static void FWhileVsDoWhile()
         {
             int x = 10;
